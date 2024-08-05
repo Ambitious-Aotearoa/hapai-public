7ccf2e94c2b8594315c3b2f0868a56c35db2612b
jennifer <jennifer@ambitious.co.nz>
2024-08-06 09:57:10 +1200

    basic svg mask

--- js/main.js
+++ js/main.js
@@ -9,8 +9,41 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 // Place any custom JS here
 //
 
-// Create an example popover
-document.querySelectorAll('[data-bs-toggle="popover"]')
-  .forEach(popover => {
-    new bootstrap.Popover(popover)
-  })
+// All path elements in the page
+const paths = [...document.querySelectorAll('path.path-anim')];
+	
+ // use a script tag or an external JS file
+ document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(ScrollTrigger)
+    // Animate the d attribute (path initial ) to the value in data-path-to;
+    // start when the top of its SVG reaches the bottom of the viewport and 
+    // end when the bottom of its SVG reaches the top of the viewport 
+    paths.forEach(el => {
+        const svgEl = el.closest('svg');
+        const pathTo = el.dataset.pathTo;
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: svgEl,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true,
+                markers:true
+            }
+        })
+        .to(el, {
+            ease: 'none',
+            attr: { d: pathTo }
+        });
+    })
+   });
+
+
+
+// // Create an example popover
+// document.querySelectorAll('[data-bs-toggle="popover"]')
+//   .forEach(popover => {
+//     new bootstrap.Popover(popover)
+//   })
+
+
