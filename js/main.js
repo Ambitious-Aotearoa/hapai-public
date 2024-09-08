6d417816233dd00ed6ce32cba73d9a20616dc471
jennifer <jennifer@ambitious.co.nz>
2024-09-08 18:13:25 +1200

    sunday

--- js/main.js
+++ js/main.js
@@ -5,9 +5,25 @@
   import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // // Create an example popover
-// document.querySelectorAll('[data-bs-toggle="popover"]')
-//   .forEach(popover => {
-//     new bootstrap.Popover(popover)
-//   })
+document.querySelectorAll('[data-bs-toggle="popover"]')
+  .forEach(popover => {
+    new bootstrap.Popover(popover)
+  })
 
 
+  $(document).ready(function() {
+    // When the collapse menu is shown
+    $('.navbar-collapse').on('shown.bs.collapse', function() {
+      $('.navbar').addClass('nav-change');
+    });
+  
+    // When the collapse menu is hidden
+    $('.navbar-collapse').on('hidden.bs.collapse', function() {
+      $('.navbar').removeClass('nav-change');
+    });
+  });
+  
+
+  
+  console.log('test')
+  
