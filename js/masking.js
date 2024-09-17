7e6e63aaa856623fd0302a222ca4ba51fd64bf92
jennifer <jennifer@ambitious.co.nz>
2024-09-17 16:51:26 +1200

    end of day

--- js/masking.js
+++ js/masking.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
     var navHeight = 0;
-    // Add click event listener to the buttons
     $('#filter button').on('click', function() {
         var filterValue = $(this).data('filter');
+
+        $('#filter button').removeClass('active');
+        $(this).addClass('active');
+
+        
         $('.filter-wrapper-other .filter-item').removeClass('show');
         $('.filter-wrapper-other .filter-item.' + filterValue).addClass('show');
 
 
-        var buttonTop = $('.content-filter').offset().top;
-        $("html, body").animate({
-            scrollTop: buttonTop - navHeight
-        }, 300);
+        // var buttonTop = $('.content-filter').offset().top;
+        // $("html, body").animate({
+        //     scrollTop: buttonTop - navHeight
+        // }, 300);
     });
 });
 
