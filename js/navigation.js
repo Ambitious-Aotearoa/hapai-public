2974198f2c9d5a1bb8e1e637a00039f5a54068a1
Jennnnifer <jennnnifer.harper@gmail.com>
2024-09-22 19:57:20 +1200

    tweaks

--- js/navigation.js
+++ js/navigation.js
@@ -1,28 +1,26 @@
-$(document).ready(function() {
-    // When the collapse menu starts to show (animation begins)
-    $('.navbar-collapse').on('show.bs.collapse', function() {
-      $('.navbar').addClass('nav-change');
-    });
-  
-    // When the collapse menu starts to hide (animation begins)
-    $('.navbar-collapse').on('hide.bs.collapse', function() {
-      $('.navbar').removeClass('nav-change');
-    });
-});
-  
+$(document).ready(function () {
+  // When the collapse menu starts to show (animation begins)
+  $('.navbar-collapse').on('show.bs.collapse', function () {
+    $('.navbar').addClass('nav-change')
+  })
 
-const burger = document.getElementById('burger');
-burger.addEventListener('click', function() {
-  this.classList.toggle('cross');
-});
+  // When the collapse menu starts to hide (animation begins)
+  $('.navbar-collapse').on('hide.bs.collapse', function () {
+    $('.navbar').removeClass('nav-change')
+  })
+})
 
+const burger = document.getElementById('burger')
+const toggler = document.querySelector('.navbar-toggler')
+toggler.addEventListener('click', function () {
+  burger.classList.toggle('cross')
+})
 
-function getNavHeight(){
-    var navHeight = $('nav').outerHeight()
-    console.log(navHeight)
-    $('.hero').css('padding-top', navHeight)
-    // $('.modal:not(#cartSummary)').css('padding-top', navHeight)
-
+function getNavHeight() {
+  var navHeight = $('nav').outerHeight()
+  console.log(navHeight)
+  $('.hero').css('padding-top', navHeight)
+  // $('.modal:not(#cartSummary)').css('padding-top', navHeight)
 }
 getNavHeight()
 window.addEventListener('resize', getNavHeight)
