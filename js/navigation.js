f219a767695aff1df91d9a97ac0b373f7aa45e9c
jennifer <jennifer@ambitious.co.nz>
2024-09-23 11:22:00 +1200

    nav updates

--- js/navigation.js
+++ js/navigation.js
@@ -20,7 +20,20 @@ function getNavHeight() {
   var navHeight = $('nav').outerHeight()
   console.log(navHeight)
   $('.hero').css('padding-top', navHeight)
-  // $('.modal:not(#cartSummary)').css('padding-top', navHeight)
 }
 getNavHeight()
 window.addEventListener('resize', getNavHeight)
+
+
+      // Select the collapse element
+      const navCollapse = document.getElementById('navbarNavDropdown')
+
+      // When the collapse is shown, add the no-scroll class to body and prevent touch scrolling outside navCollapse
+      navCollapse.addEventListener('show.bs.collapse', function () {
+        document.body.classList.add('no-scroll')
+      })
+
+      // When the collapse is hidden, remove the no-scroll class and re-enable body touch scrolling
+      navCollapse.addEventListener('hidden.bs.collapse', function () {
+        document.body.classList.remove('no-scroll')
+      })
