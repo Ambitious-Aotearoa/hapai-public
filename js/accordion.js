567faca025888180ab5be751db923a7df681fd55
jennifer <jennifer@ambitious.co.nz>
2024-09-23 15:44:26 +1200

    nav and footer changes for insights and resources page

--- js/accordion.js
+++ js/accordion.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
-  var navHeight = $('nav').outerHeight()
-  console.log(navHeight)
-  $('.accordion-item').on('click', function () {
-    var buttonTop = $(this).offset().top
-    $('html, body').animate(
-      {
-        scrollTop: buttonTop + navHeight + 60,
-      },
-      300
-    )
-  })
-})
+    var navHeight = $('nav').outerHeight();
+    console.log(navHeight);
+  
+    // Trigger scroll animation immediately on click
+    $('.accordion-item').on('click', function () {
+      var buttonTop = $(this).offset().top;
+  
+      // Scroll the page immediately when the button is clicked
+      $('html, body').animate(
+        {
+          scrollTop: buttonTop - navHeight, // Adjust for nav height
+        },
+        300 // Duration of the scroll animation
+      );
+    });
+  });
+  
--- js/filter.js
+++ js/filter.js
@@ -5,19 +5,14 @@ $(document).ready(function () {
         // Initialize filters for each container
         $('[id^="btnContainer"]').each(function () {
             var containerId = $(this).attr('id');
-            console.log('Initializing filter for container:', containerId);
 
             // Add event listeners to buttons within each container
             $('#' + containerId + ' .btn').click(function () {
-                console.log('Button clicked in container:', containerId);
-                $('#' + containerId + ' .btn').removeClass('active'); // Remove active class from all buttons in the container
-                $(this).addClass('active'); // Add active class to the clicked button
+                $('#' + containerId + ' .btn').removeClass('active'); 
+                $(this).addClass('active'); 
 
                 var filter = $(this).data('filter');
-                console.log('Filtering with criteria:', filter);
                 filterSelection(containerId, filter);
-
-                // Call the scroll function based on the filter
                 scrollToFilter(containerId, filter);
             });
         });
@@ -25,17 +20,15 @@ $(document).ready(function () {
 });
 
 function scrollToFilter(containerId, filter) {
-    var navHeight = 0 // Adjust this if you have a fixed header
+    var navHeight = $('nav').outerHeight()
 
     if (filter === "all") {
-        // Scroll to the top of the filter wrapper, adjusted for the fixed header
         $("html, body").animate({
             scrollTop: $('#' + containerId).next('.filter-wrapper').offset().top - navHeight
         }, 300);
     } else {
         var target = $('#' + containerId).next('.filter-wrapper').find('.filter-item.' + filter).first();
         if (target.length) {
-            // Scroll to the target item, adjusted for the fixed header
             $("html, body").animate({
                 scrollTop: target.offset().top - navHeight
             }, 300);
