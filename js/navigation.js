989995ffe72128f2e613c045193a5b693fdb52cc
jennifer <jennifer@ambitious.co.nz>
2024-09-24 14:33:18 +1200

    partners and identity page

--- js/navigation.js
+++ js/navigation.js
@@ -1,39 +1,18 @@
 $(document).ready(function () {
-  // When the collapse menu starts to show (animation begins)
-  $('.navbar-collapse').on('show.bs.collapse', function () {
-    $('.navbar').addClass('nav-change')
-  })
-
-  // When the collapse menu starts to hide (animation begins)
-  $('.navbar-collapse').on('hide.bs.collapse', function () {
-    $('.navbar').removeClass('nav-change')
-  })
-})
-
-const burger = document.getElementById('burger')
-const toggler = document.querySelector('.navbar-toggler')
-toggler.addEventListener('click', function () {
-  burger.classList.toggle('cross')
-})
-
-// function getNavHeight() {
-//   var navHeight = $('nav').outerHeight()
-//   console.log(navHeight)
-//   $('.hero').css('padding-top', navHeight)
-// }
-// getNavHeight()
-// window.addEventListener('resize', getNavHeight)
-
-
-      // Select the collapse element
-      const navCollapse = document.getElementById('navbarNavDropdown')
-
-      // When the collapse is shown, add the no-scroll class to body and prevent touch scrolling outside navCollapse
-      navCollapse.addEventListener('show.bs.collapse', function () {
-        document.body.classList.add('no-scroll')
-      })
-
-      // When the collapse is hidden, remove the no-scroll class and re-enable body touch scrolling
-      navCollapse.addEventListener('hidden.bs.collapse', function () {
-        document.body.classList.remove('no-scroll')
-      })
+    const navbar = $('.navbar');
+    const burger = $('#burger'); // jQuery selector for consistency
+    const toggler = $('.navbar-toggler');
+    const navCollapse = $('#navbarNavDropdown'); // jQuery selector
+  
+    // Add or remove classes when the collapse menu is toggled
+    $('.navbar-collapse').on('show.bs.collapse', () => navbar.addClass('nav-change'));
+    $('.navbar-collapse').on('hide.bs.collapse', () => navbar.removeClass('nav-change'));
+  
+    // Toggle burger icon to a cross
+    toggler.on('click', () => burger.toggleClass('cross'));
+  
+    // Prevent scrolling when the navbar is expanded
+    navCollapse.on('show.bs.collapse', () => $('body').addClass('no-scroll'));
+    navCollapse.on('hidden.bs.collapse', () => $('body').removeClass('no-scroll'));
+  });
+  
