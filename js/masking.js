021724e0af975a047ef34faab977ba03648353d6
jennifer <jennifer@ambitious.co.nz>
2024-09-27 11:12:13 +1200

    scroll animations done

--- js/masking.js
+++ js/masking.js
@@ -1,24 +1,3 @@
-$(document).ready(function() {
-    var navHeight = 0;
-    $('#filter button').on('click', function() {
-        var filterValue = $(this).data('filter');
-
-        $('#filter button').removeClass('active');
-        $(this).addClass('active');
-
-        
-        $('.filter-wrapper-other .filter-item').removeClass('show');
-        $('.filter-wrapper-other .filter-item.' + filterValue).addClass('show');
-
-
-        // var buttonTop = $('.content-filter').offset().top;
-        // $("html, body").animate({
-        //     scrollTop: buttonTop - navHeight
-        // }, 300);
-    });
-});
-
-
 
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
@@ -30,6 +9,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         var firstHeight = $('.mask-item.one h3').outerHeight() + 2;
         var secondHeight = $('.mask-item.two h3').outerHeight() + 2;
         var thirdHeight = $('.mask-item.three h3').outerHeight() + 2;
+        var fourthHeight = $('.mask-item.four h3').outerHeight() + 2;
 
         let tl = gsap.timeline({
             defaults: {ease: 'none' },
@@ -47,13 +27,23 @@ document.addEventListener("DOMContentLoaded", (event) => {
         })
 
         gsap.set('.mask-wrapper',{ yPercent:10})
-        gsap.set('.btn-wrapper',{ yPercent:200})
+
+        if ($('.btn-wrapper').length) {
+            gsap.set('.btn-wrapper', { yPercent: 200 });
+        }
+
         gsap.set('.mask-item.three  p',{ opacity:0})
         gsap.set('.mask-item.two  p',{ opacity:0})
         gsap.set('.mask-item.one',{ ease: "circ.out"})
 
         gsap.set('.mask-item.two',{ height:secondHeight, ease: "circ.out"})
         gsap.set('.mask-item.three',{ height:thirdHeight, ease: "circ.out"})
+
+        if ($('.mask-item.four').length) {
+            gsap.set('.mask-item.four  p',{ opacity:0})
+            gsap.set('.mask-item.four',{ height:fourthHeight, ease: "circ.out"})
+        }
+
 
 
         tl.addLabel("start");
@@ -66,15 +56,33 @@ document.addEventListener("DOMContentLoaded", (event) => {
         tl.to('.mask-item.one p', 0.5 ,{opacity:0}, "second")
         tl.to('.mask-item.two p', {opacity:1}, "second+=0.5")
        
-        tl.addLabel("three", 18);
+        tl.addLabel("three", 24);
         tl.to('.mask-item.three', 6,{height:'auto',  ease: "circ.out"},"three")
         tl.to('.mask-item.two', 6,{height:secondHeight,  ease: "circ.out"},"three")
         tl.to('.mask-img.two',{ scale:1.2, opacity:0},"three")
         tl.to('.mask-item.two p', 0.5, {opacity:0}, "three")
         tl.to('.mask-item.three p', {opacity:1}, "three+=0.5")
 
-        tl.addLabel("end", 28);
-        tl.to('.btn-wrapper',4,{ yPercent:0},"end")
+       
+        if ($('.btn-wrapper').length && !$('.mask-item.four').length) {
+            tl.addLabel("end", 42);
+            tl.to('.btn-wrapper', 4, { yPercent: 0 }, "end");
+        }
+
+
+
+        if ($('.mask-item.four').length) {
+            tl.addLabel("four", 42);
+            tl.to('.mask-item.four', 6,{height:'auto',  ease: "circ.out"},"four")
+            tl.to('.mask-item.three', 6,{height:secondHeight,  ease: "circ.out"},"four")
+            tl.to('.mask-img.three',{ scale:1.2, opacity:0},"four")
+            tl.to('.mask-item.three p', 0.5, {opacity:0}, "four")
+            tl.to('.mask-item.four p', {opacity:1}, "four+=0.5")
+            tl.addLabel("end", 54);
+            tl.to('.btn-wrapper', 4, { yPercent: 0 }, "end");
+        }
+
+
     }
 
 
