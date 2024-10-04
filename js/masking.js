41b760b4a5aef00888fb09be2ca141897a4952ac
jennifer <jennifer@ambitious.co.nz>
2024-10-04 15:15:02 +1300

    animation change

--- js/masking.js
+++ js/masking.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
 });
 
 
+
+
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger)
     let mm = gsap.matchMedia()
@@ -19,92 +21,34 @@ document.addEventListener("DOMContentLoaded", (event) => {
     var marker = { startColor: 'green', endColor: 'red', fontSize: '18px', fontWeight: 'bold', indent: 20, zIndex: 2000 }
 
     function maskingAnimation(){
-        var firstHeight = $('.mask-item.one h3').outerHeight() + 2;
-        var secondHeight = $('.mask-item.two h3').outerHeight() + 2;
-        var thirdHeight = $('.mask-item.three h3').outerHeight() + 2;
-        var fourthHeight = $('.mask-item.four h3').outerHeight() + 2;
-
         let tl = gsap.timeline({
             defaults: {ease: 'none' },
             scrollTrigger: {
-              trigger: '.masking-trigger',
+              trigger: '.animated-section',
               pin: true,
               anticipatePin: 1,
               scrub: 1,
               start: 'top top+=' + navHeight,
-              end:'+=500%',
+              end: "+=300%",
               invalidateOnRefresh: true,
-            // toggleActions: 'play none none reverse',
-            //   markers: marker,
+                // toggleActions: 'play none none reverse',
+              markers: marker,
             },
         })
 
-        gsap.set('.mask-wrapper',{ yPercent:10})
-
-        if ($('.btn-wrapper').length) {
-            gsap.set('.btn-wrapper', { yPercent: 200, opacity:0 });
-        }
-
-        gsap.set('.mask-item.three  p',{ opacity:0})
-        gsap.set('.mask-item.two  p',{ opacity:0})
-        gsap.set('.mask-item.one',{ ease: "circ.out"})
-
-        gsap.set('.mask-item.two',{ height:secondHeight, ease: "circ.out"})
-        gsap.set('.mask-item.three',{ height:thirdHeight, ease: "circ.out"})
-
-        if ($('.mask-item.four').length) {
-            gsap.set('.mask-item.four  p',{ opacity:0})
-            gsap.set('.mask-item.four',{ height:fourthHeight, ease: "circ.out"})
-        }
-
-
-
         tl.addLabel("start");
-        tl.to('.mask-wrapper',{ yPercent:0, ease: "circ.out", duration:3}, "start")
-
-        tl.addLabel("second", 100);
-        tl.to('.mask-item.one', 10,{height:firstHeight, ease: "circ.out"},"second")
-        tl.to('.mask-item.one p', 4,{opacity:0, yPercent:-50}, "second-=1")
-        tl.to('.mask-item.two', 4,{height:'auto', ease: "circ.out"},"second")
-        tl.to('.mask-item.two p', 4,{opacity:1}, "second+=0.5")
-        tl.to('.mask-img.one', 2,{ scale:1.2, opacity:0},"second")
-
-     
-       
-        tl.addLabel("three", 200);
-        tl.to('.mask-item.two', 10,{height:secondHeight,  ease: "circ.out"},"three")
-        tl.to('.mask-item.two p', 4,{opacity:0, yPercent:-50}, "three-=1")
-        tl.to('.mask-item.three', 4,{height:'auto',  ease: "circ.out"},"three")
-        tl.to('.mask-item.three p', 4, {opacity:1}, "three+=0.5")
-        tl.to('.mask-img.two',2, { scale:1.2, opacity:0},"three")
-
-       
-        if ($('.btn-wrapper').length && !$('.mask-item.four').length) {
-            tl.addLabel("end", 300);
-            tl.to('.btn-wrapper', 1, { yPercent: 0, opacity:1 }, "end");
-        }
-
-
-
-        if ($('.mask-item.four').length) {
-            tl.addLabel("four", 300);
-
-            tl.to('.mask-item.three', 10,{height:secondHeight,  ease: "circ.out"},"four")
-            tl.to('.mask-item.three p', 4, {opacity:0, yPercent:-50}, "four-=1")
-            tl.to('.mask-item.four', 4,{height:'auto',  ease: "circ.out"},"four")
-            tl.to('.mask-item.four p', 4,{opacity:1}, "four+=0.5")
-            tl.to('.mask-img.three',2, { scale:1.2, opacity:0},"four")
-
-
-            tl.addLabel("end", 320);
-            tl.to('.btn-wrapper', 4, { yPercent: 0 }, "end");
-        }
+        tl.to(".animated-section .--blue", {top:0, duration:2}, 'start');
+        tl.addLabel("middle");
+        tl.to(".animated-section .--yellow", {top:0, duration:2}, 'middle+=2');
+        tl.addLabel("end");
+        tl.to(".animated-section .--yellow", {position:'fixed'}, 'end+=2');
     }
-
+    
 
     mm.add('(max-width: 1023px)', () => {
+        gsap.set(".animated-section .--blue", {top:'unset'});
+        gsap.set(".animated-section .--yellow", {top:'unset', position:'relative'});
         ScrollTrigger.refresh()
-        gsap.set('.mask-item.one',{ height:'auto', ease: "circ.out"})
     })
 
     mm.add('(min-width: 1024px)', () => {
@@ -114,19 +58,125 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 
     window.addEventListener('resize', () => {
-        mm.revert(); // Revert any existing matchMedia conditions
-        mm.add('(max-width: 1023px)', () => {
-            gsap.set('.mask-item.one', { height: 'auto', ease: "circ.out" });
-        });
-
-        mm.add('(min-width: 1024px)', () => {
-            maskingAnimation();
-        });
-
         ScrollTrigger.refresh(); // Refresh ScrollTrigger to ensure everything is recalculated
     });
-
-
-
+ 
 })
 
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     gsap.registerPlugin(ScrollTrigger)
+//     let mm = gsap.matchMedia()
+//     var navHeight = $('nav').outerHeight();
+//     var marker = { startColor: 'green', endColor: 'red', fontSize: '18px', fontWeight: 'bold', indent: 20, zIndex: 2000 }
+
+//     function maskingAnimation(){
+//         var firstHeight = $('.mask-item.one h3').outerHeight() + 2;
+//         var secondHeight = $('.mask-item.two h3').outerHeight() + 2;
+//         var thirdHeight = $('.mask-item.three h3').outerHeight() + 2;
+//         var fourthHeight = $('.mask-item.four h3').outerHeight() + 2;
+
+//         let tl = gsap.timeline({
+//             defaults: {ease: 'none' },
+//             scrollTrigger: {
+//               trigger: '.masking-trigger',
+//               pin: true,
+//               anticipatePin: 1,
+//               scrub: 1,
+//               start: 'top top+=' + navHeight,
+//               end:'+=500%',
+//               invalidateOnRefresh: true,
+//             // toggleActions: 'play none none reverse',
+//             //   markers: marker,
+//             },
+//         })
+
+//         gsap.set('.mask-wrapper',{ yPercent:10})
+
+//         if ($('.btn-wrapper').length) {
+//             gsap.set('.btn-wrapper', { yPercent: 200, opacity:0 });
+//         }
+
+//         gsap.set('.mask-item.three  p',{ opacity:0})
+//         gsap.set('.mask-item.two  p',{ opacity:0})
+//         gsap.set('.mask-item.one',{ ease: "circ.out"})
+
+//         gsap.set('.mask-item.two',{ height:secondHeight, ease: "circ.out"})
+//         gsap.set('.mask-item.three',{ height:thirdHeight, ease: "circ.out"})
+
+//         if ($('.mask-item.four').length) {
+//             gsap.set('.mask-item.four  p',{ opacity:0})
+//             gsap.set('.mask-item.four',{ height:fourthHeight, ease: "circ.out"})
+//         }
+
+
+
+//         tl.addLabel("start");
+//         tl.to('.mask-wrapper',{ yPercent:0, ease: "circ.out", duration:3}, "start")
+
+//         tl.addLabel("second", 100);
+//         tl.to('.mask-item.one', 10,{height:firstHeight, ease: "circ.out"},"second")
+//         tl.to('.mask-item.one p', 4,{opacity:0, yPercent:-50}, "second-=1")
+//         tl.to('.mask-item.two', 4,{height:'auto', ease: "circ.out"},"second")
+//         tl.to('.mask-item.two p', 4,{opacity:1}, "second+=0.5")
+//         tl.to('.mask-img.one', 2,{ scale:1.2, opacity:0},"second")
+
+     
+       
+//         tl.addLabel("three", 200);
+//         tl.to('.mask-item.two', 10,{height:secondHeight,  ease: "circ.out"},"three")
+//         tl.to('.mask-item.two p', 4,{opacity:0, yPercent:-50}, "three-=1")
+//         tl.to('.mask-item.three', 4,{height:'auto',  ease: "circ.out"},"three")
+//         tl.to('.mask-item.three p', 4, {opacity:1}, "three+=0.5")
+//         tl.to('.mask-img.two',2, { scale:1.2, opacity:0},"three")
+
+       
+//         if ($('.btn-wrapper').length && !$('.mask-item.four').length) {
+//             tl.addLabel("end", 300);
+//             tl.to('.btn-wrapper', 1, { yPercent: 0, opacity:1 }, "end");
+//         }
+
+
+
+//         if ($('.mask-item.four').length) {
+//             tl.addLabel("four", 300);
+
+//             tl.to('.mask-item.three', 10,{height:secondHeight,  ease: "circ.out"},"four")
+//             tl.to('.mask-item.three p', 4, {opacity:0, yPercent:-50}, "four-=1")
+//             tl.to('.mask-item.four', 4,{height:'auto',  ease: "circ.out"},"four")
+//             tl.to('.mask-item.four p', 4,{opacity:1}, "four+=0.5")
+//             tl.to('.mask-img.three',2, { scale:1.2, opacity:0},"four")
+
+
+//             tl.addLabel("end", 320);
+//             tl.to('.btn-wrapper', 4, { yPercent: 0 }, "end");
+//         }
+//     }
+
+
+//     mm.add('(max-width: 1023px)', () => {
+//         ScrollTrigger.refresh()
+//         gsap.set('.mask-item.one',{ height:'auto', ease: "circ.out"})
+//     })
+
+//     mm.add('(min-width: 1024px)', () => {
+//         ScrollTrigger.refresh()
+//         maskingAnimation()
+//     })
+
+
+//     window.addEventListener('resize', () => {
+//         mm.revert(); // Revert any existing matchMedia conditions
+//         mm.add('(max-width: 1023px)', () => {
+//             gsap.set('.mask-item.one', { height: 'auto', ease: "circ.out" });
+//         });
+
+//         mm.add('(min-width: 1024px)', () => {
+//             maskingAnimation();
+//         });
+
+//         ScrollTrigger.refresh(); // Refresh ScrollTrigger to ensure everything is recalculated
+//     });
+
+
+
+// })
